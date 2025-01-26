ad510cf0257e97a11d8a7ad57fcbd541b3358bd3
M. Huda <mhuda@users-MacBook-Pro.local>
2025-01-26 15:51:48 +0700

    add

--- modules/models/clientapplicationmodel.js
+++ modules/models/clientapplicationmodel.js
@@ -7,7 +7,9 @@ class ClientApplicationModel extends Model {
             appID: DataTypes.STRING,
             apiKey: DataTypes.STRING,
             description: DataTypes.TEXT,
-            createdBy: DataTypes.STRING
+            createdBy: DataTypes.STRING,
+            appType: DataTypes.STRING,
+            tag: DataTypes.STRING
         }, 
         { sequelize, modelName: 'clientapplication', tableName: 'clientapplication', force: force });
     }
